496c07f9cd972dbcf769771479bafa1c88b229a6
panzerstadt <tliqun@gmail.com>
2022-04-04 23:20:00 +0900

    transition using states first vs event first

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
 import './style.css';
 import { createMachine } from 'xstate';
+
+
+
+// state: { status: 'idle' }
+// event: { type: "FETCH" }
+
+// like a reducer
+// given current state + event -> next state
+function transition(state, event) {
+    switch (state.status) {
+        case 'idle':
+            if (event.type === 'FETCH') {
+                console.log("starting to fetch data");
+                return { status: "loading" };
+            }
+            return state;
+        case 'loading':
+            // other behaviour
+            return state;
+        default:
+            console.log("nothing happens");
+            return state;
+    }
+}
+
+window.transition = transition
