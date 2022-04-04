685ba517eaae746691a1c24d4d2198b657a596b4
panzerstadt <tliqun@gmail.com>
2022-04-05 02:16:23 +0900

    hand rolled state machine

--- 00-modeling/main.js
+++ 00-modeling/main.js
@@ -75,30 +75,110 @@ window.thing2 = thing2;
 
 
 // object map style, minimal
+let timer;
+let songProgress = 0;
+let songLength = 0;
+let songName = 'Some Song'
+const newSong = () => {
+    songLength = Math.max(Math.floor(Math.random() * 30), 10);
+    songName = Array.from(songName).reverse().join("");
+    songProgress = 0;
+
+    document.getElementById("scrubber").max = songLength
+    document.getElementById("elapsed").innerHTML = songProgress
+    document.getElementById("length").innerHTML = " / " + songLength
+    document.getElementById("song-title").innerHTML = songName
+}
+const tick = () => {
+    timer = setInterval(() => {
+        songProgress++;
+        if (songProgress > songLength) {
+            thing3.send({ type: "NEXT" })
+        }
+        document.getElementById("elapsed").innerHTML = songProgress;
+        document.getElementById("scrubber").value = songProgress;
+    }, 1000)
+}
+const stop = () => {
+    clearInterval(timer)
+}
+
+
 const machine = {
     initial: "loading",
     states: {
         loading: {
             on: {
-                LOADED: 'playing',
-                action: () => {
-                    alert("wow imma start playing yowza")
-                    document.getElementById("button-loading").hidden = true
-                }
+                LOADED: {
+                    target: 'playing',
+                    action: () => {
+                        alert("wow imma start playing yowza")
+                        document.getElementById("button-loading").hidden = true;
+                        newSong()
+                        tick()
+                    }
+                },
+
             }
         },
         playing: {
             on: {
-                PAUSE: 'pausing',
-                action: () => { alert("i pausing har") }
+                PAUSE: {
+                    target: 'pausing',
+                    action: () => {
+                        stop()
+                    }
+                },
+                NEXT: {
+                    target: 'playing',
+                    action: () => {
+                        newSong()
+                    }
+                },
+                SCRUB_START: {
+                    target: 'scrubbing',
+                    action: () => {
+                        stop()
+                    }
+                }
+            }
+        },
+        scrubbing: {
+            on: {
+                SCRUB_END: {
+                    target: 'playing', // could also 'remember' last player state?
+                    action: (payload) => {
+                        if (payload.progress) {
+                            songProgress = payload.progress
+                            document.getElementById("elapsed").innerHTML = songProgress
+                        }
+                        tick()
+                    }
+                },
             }
         },
         pausing: {
             on: {
-                PLAY: "playing",
-                action: () => { alert("imma continue playing!") }
+                PLAY: {
+                    target: "playing",
+                    action: () => {
+                        tick()
+                    }
+                },
+                NEXT: {
+                    target: 'playing',
+                    action: () => {
+                        newSong()
+                    }
+                },
+                SCRUB_START: {
+                    target: 'scrubbing',
+                    action: () => {
+                        stop()
+                    }
+                }
             }
-        }
+        },
     }
 }
 
@@ -108,11 +188,18 @@ const thing3 = {
         const nextState = machine.states[this.state].on?.[event.type]
 
         if (nextState) {
-            machine.states[this.state].on?.action?.() // fire side effect if exists
-            console.log("next state: ", nextState);
-            this.state = nextState;
+            // fire side effect if exists
+            nextState.action?.(event.payload)
+
+            // log stuff
+            console.log("event:", event.type, " next state:", nextState);
+            document.getElementById('state').innerHTML = nextState.target;
+
+            // update state in machine
+            this.state = nextState.target;
         }
     }
 }
 window.thing3 = thing3;
+document.getElementById('state').innerHTML = thing3.state;
 // while playing, thing3.send({type: "PAUSE", action: () => {alert("WOW")}})
