c1aeecbaf56fbbcbfe99b8b52bc3e8ec6c60aa94
panzerstadt <tliqun@gmail.com>
2022-04-05 00:51:46 +0900

    tomfoolery

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import './style.css';
 import { createMachine } from 'xstate';
 
-
+/**
+ * switch statement style
+ */
 
 // state: { status: 'idle' }
 // event: { type: "FETCH" }
@@ -24,5 +26,78 @@ function transition(state, event) {
             return state;
     }
 }
-
 window.transition = transition
+
+// TODO: doesn't this also work the same?
+function eventFirstTransition(state, event) {
+    switch (event.type) {
+        case "FETCH":
+            if (state.status === 'idle') {
+                console.log("starting to fetch data");
+                return { status: "loading" };
+            }
+        default:
+            return state
+    }
+}
+window.eventTransition = eventFirstTransition
+
+/**
+ * object lookup style
+ * 
+ * simple rules
+ * - states must have status
+ * - events must have types
+ */
+const machine = {
+    initial: "idle",
+    states: {
+        idle: {
+            on: {
+                FETCH: "loading"
+            }
+        },
+        loading: {}
+    }
+}
+function machineTransition(state, event) {
+    const nextStatus = machine.states[state.status].on?.[event.type] ?? state.status
+
+    return {
+        status: nextStatus
+    }
+}
+/**
+ * try:
+ * machineTransition({ status: machine.initial },{ type:"FETCH" })
+ */
+window.machine = machine
+window.machineTransition = machineTransition
+
+
+// this is the same thing, but flipped.
+// it all boils down to english grammar.
+const eventMachine = {
+    initial: "DONE",
+    events: {
+        FETCH: {
+            whileStatus: {
+                loading: "DONE",
+                idle: "CANCEL"
+            }
+        },
+        DONE: {
+            whileStatus: {
+                idle: "FETCH"
+            }
+        }
+    }
+}
+function eventMachineTransition(state, event) {
+    const nextEvent = eventMachine.events[event.type].whileStatus?.[state.status] ?? event.type
+
+    return {
+        event: nextEvent
+    }
+}
+window.eventMachineTransition = eventMachineTransition
